193874d6ee5eceba153576ebe822eed58ce8d5e7
CodeWithArafat1 <arafatnill2.0@gmail.com>
2025-07-12 16:12:20 +0600

    variables

---
+++ chapter_001/script.js
@@ -0,0 +1,47 @@
+// ! 003. How to Use This Course Contents | JS All You Need to Know | JS Bangla Tutorials
+// ? javaScript and Node.js by default asynchronous programign
+// ? DOM javaScript na DOM is just an Aplication or Libary
+
+// ! 005. First Code of JS | JS All You Need to Know | JS Bangla Tutorials
+// ? Q: What is Programming
+// ? A: Programming is a set of instruction which our computer can read, understand and produce and output.
+
+// ? print hello world
+
+// console.log("Hello world!");
+// console.log(45.5);
+// console.log(9 + "9" + 9);
+
+//! 006. What is Variables | JS All You Need to Know | JS Bangla Tutorials
+
+// var creatorName = "Arafat Nill";
+// var age = 21;
+
+// console.log(creatorName + " knows JavaScript");
+// console.log("His age is only " + age);
+// console.log(creatorName + " is creating JavaScript course for free");
+// console.log("But this age is only " + age);
+
+//! aigula variable a use korte parbe nah
+
+// ! Standard Reserved Words
+// ? break       case        catch       class       const
+// ? continue    debugger    default     delete      do
+// ? else        export      extends     finally     for
+// ? function    if          import      in          instanceof
+// ? new         return      super       switch      this
+// ? throw       try         typeof      var         void
+// ? while       with        yield       null
+
+// ! Future Reserved Words
+// ? enum        await       implements  interface
+// ? let         package     private     protected
+// ? public      static
+
+//! variable er name always camelCase or underscore (__) use korte hobe  like ↓
+
+// var mathNumber = 21;
+// var accountNumberDetailsId = 1010;
+
+// var math_number = 21;
+// var account_number_details_id = 1010;

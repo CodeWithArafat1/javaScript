aa6553ebff76891e661347cb41015016e379eb98
CodeWithArafat1 <arafatnill2.0@gmail.com>
2025-07-16 14:22:33 +0600

    dones

---
+++ array.js
@@ -0,0 +1,41 @@
+//? *************** What is Array **********
+
+// ! Array is a Data Stucture where we can organize multiple data under a single variable
+
+// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// let length = 0;
+
+// for (;;) {
+//   if (data[length] === undefined) {
+//     break;
+//   } else {
+//     length++;
+//   }
+// }
+// console.log(length);
+// const arr = Array(1,3,4,5)
+
+//! JS Array Traversing | JS All You Need to Know | JS Bangla Tutorials
+
+const data = [3, 5, 32, 6, 4, 12, 6];
+
+// for (let i = 0; i <= data.length - 1; i++) {
+//   console.log(data[i]);
+//    data[i] = data[i] +  2;
+//   data[i] += 2;
+// }
+
+// console.log(data);
+
+// let sum = 0;
+
+// for (let i = 0; i <= data.length - 1; i++) {
+  //   sum += data[i];
+//   sum = sum + data[i];
+// }
+
+// console.log(sum);
+
+
+//! Array Insert Remove and Replace | JS All You Need to Know | JS Bangla Tutorials

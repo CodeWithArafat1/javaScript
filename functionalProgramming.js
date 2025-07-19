7ebf54ab80b93fea90685ded106ddc9d68dfbee4
CodeWithArafat1 <arafatnill2.0@gmail.com>
2025-07-19 14:00:58 +0600

    advance topic starts

---
+++ functionalProgramming.js
@@ -0,0 +1,9 @@
+//! What is Functional Programming in Javascript | JS All You Need To Know | JS Bangla Tutorials
+
+//? Functional Programming মানে হলো — **side effect ছাড়া ছোট ছোট pure function দিয়ে কোড লেখা, যেখানে একই input সবসময় একই output দেয় এবং ডেটা পরিবর্তন না করে নতুন ডেটা তৈরি করা হয়।** ✅
+
+// f ******************
+
+//? `   1. Pure functions`
+//? `   2. First Class functions`
+//? `   3. Higher Order functions`
---
+++ pure.js
@@ -0,0 +1,14 @@
+//! Pure Functions in Javascript | JS All You Need To Know | JS Bangla Tutorials
+
+// What is pure function?
+
+// * It Retruns the same result if given the same arguments
+// * It dose not cause any observable side effect
+
+function sqr(n) {
+  return n * n;
+}
+
+console.log(sqr(3));
+console.log(sqr(3));
+console.log(sqr(3));
